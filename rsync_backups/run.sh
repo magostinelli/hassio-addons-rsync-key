876e84938f38dc8b03d0227a9b0ae95b64eb5a3f
Michele Agostinelli <michele@agostinelli.eu>
2023-12-05 14:03:44 +0100

    Update run.sh

--- rsync_backups/run.sh
+++ rsync_backups/run.sh
@@ -1,26 +1,35 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bashio
+export CONFIG_PATH=/data/options.json
+export server=$(jq --raw-output ".server" $CONFIG_PATH)
+export port=$(jq --raw-output ".port" $CONFIG_PATH)
+export directory=$(jq --raw-output ".directory" $CONFIG_PATH)
+export auto_purge=$(jq --raw-output ".auto_purge" $CONFIG_PATH)
+export SSH_OPT="ssh -i ~/.ssh/id_ed25519 -o UserKnownHostsFile=/data/known_hosts"
 
-echo "[Info] Starting Hass.io rsync-backups docker container!"
+PUBLIC_KEY=`cat ~/.ssh/id_ed25519.pub`
 
-CONFIG_PATH=/data/options.json
-server=$(jq --raw-output ".server" $CONFIG_PATH)
-port=$(jq --raw-output ".port" $CONFIG_PATH)
-directory=$(jq --raw-output ".directory" $CONFIG_PATH)
-username=$(jq --raw-output ".username" $CONFIG_PATH)
-password=$(jq --raw-output ".password" $CONFIG_PATH)
-auto_purge=$(jq --raw-output ".auto_purge" $CONFIG_PATH)
+bashio::log.info "A public/private key pair was generated for you."
+bashio::log.notice "Please use this public key on the backup server:"
+bashio::log.notice "${PUBLIC_KEY}"
+
+if [ ! -f /data/known_hosts ]; then
+   bashio::log.info "Running for the first time, acquiring host key and storing it in /data/known_hosts."
+   ssh-keyscan -p $(bashio::config 'port') "$(bashio::config 'host')" > /data/known_hosts \
+     || bashio::exit.nok "Could not acquire host key from backup server."
+fi
 
 rsyncurl="$username@$server:$directory"
 
-echo "[Info] Start rsync-backups"
-
-echo "[Info] Start rsync backups to $rsyncurl"
-sshpass -p $password rsync -av -e "ssh -p $port -o StrictHostKeyChecking=no" /backup/ $rsyncurl
+bashio::log.info 'Uploading backup to to $rsyncurl ...'
+/ursync -av -e $SSH_OPT /backup/ $rsyncurl \
+  || bashio::exit.nok "Could not upload backup."
 
 if [ $auto_purge -ge 1 ]; then
-	echo "[Info] Start auto purge, keep last $auto_purge backups"
-	rm `ls -t /backup/*.tar | awk "NR>$auto_purge"`
+	bashio::log.info 'Start auto purge, keep last $auto_purge backups'
+	rm `ls -t /backup/*.tar | awk "NR>$auto_purge"` || bashio::exit.nok "Could not prune backups."
 fi
 
-echo "[Info] Finished rsync-backups"
+bashio::log.info 'Finished rsync-backups'
+
+bashio::log.info 'Finished.'
+bashio::exit.ok
