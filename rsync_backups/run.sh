769218a42fb099f48455477ccdb9ec5af826bef9
Michele Agostinelli <michele@agostinelli.eu>
2023-12-05 15:01:53 +0100

    Update run.sh

--- rsync_backups/run.sh
+++ rsync_backups/run.sh
@@ -4,19 +4,13 @@ export server=$(jq --raw-output ".server" $CONFIG_PATH)
 export port=$(jq --raw-output ".port" $CONFIG_PATH)
 export directory=$(jq --raw-output ".directory" $CONFIG_PATH)
 export auto_purge=$(jq --raw-output ".auto_purge" $CONFIG_PATH)
-export SSH_OPT="ssh -i ~/.ssh/id_ed25519 -o UserKnownHostsFile=/data/known_hosts"
+export SSH_OPT="ssh -i ~/.ssh/id_ed25519 -oStrictHostKeyChecking=no"
 
 PUBLIC_KEY=`cat ~/.ssh/id_ed25519.pub`
 
 bashio::log.info "A public/private key pair was generated for you."
 bashio::log.notice "Please use this public key on the backup server:"
 bashio::log.notice "${PUBLIC_KEY}"
-
-if [ ! -f /data/known_hosts ]; then
-   bashio::log.info "Running for the first time, acquiring host key and storing it in /data/known_hosts."
-   ssh-keyscan -p $(bashio::config 'port') "$(bashio::config 'host')" > /data/known_hosts \
-     || bashio::exit.nok "Could not acquire host key from backup server."
-fi
 
 rsyncurl="$server:$directory"
 
