378938f3c1601fe164b86e2decc6f3f3af45b68b
Michele Agostinelli <michele@agostinelli.eu>
2023-12-05 15:42:14 +0100

    Update run.sh

--- rsync_backups/run.sh
+++ rsync_backups/run.sh
@@ -2,6 +2,7 @@
 export CONFIG_PATH=/data/options.json
 export server=$(jq --raw-output ".server" $CONFIG_PATH)
 export port=$(jq --raw-output ".port" $CONFIG_PATH)
+export user=$(jq --raw-output ".user" $CONFIG_PATH)
 export directory=$(jq --raw-output ".directory" $CONFIG_PATH)
 export auto_purge=$(jq --raw-output ".auto_purge" $CONFIG_PATH)
 export SSH_OPT="ssh -i ~/.ssh/id_ed25519 -o StrictHostKeyChecking=no"
@@ -12,7 +13,7 @@ bashio::log.info "A public/private key pair was generated for you."
 bashio::log.notice "Please use this public key on the backup server:"
 bashio::log.notice "${PUBLIC_KEY}"
 
-rsyncurl="$server:$directory"
+rsyncurl="$user@$server:$directory"
 
 bashio::log.info "Uploading backup to to $rsyncurl ..."
 bashio::log.debug "rsync -av -e $SSH_OPT /backup/ $rsyncurl"
