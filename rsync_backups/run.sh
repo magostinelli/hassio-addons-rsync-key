1a1f8702dfce6325aa4f6748bb1925c3b4217a64
Michele Agostinelli <michele@agostinelli.eu>
2023-12-06 11:12:56 +0100

    Update run.sh

--- rsync_backups/run.sh
+++ rsync_backups/run.sh
@@ -4,6 +4,8 @@ export server=$(jq --raw-output ".server" $CONFIG_PATH)
 export port=$(jq --raw-output ".port" $CONFIG_PATH)
 export user=$(jq --raw-output ".user" $CONFIG_PATH)
 export directory=$(jq --raw-output ".directory" $CONFIG_PATH)
+export binary_sensor=$(jq --raw-output ".binary_sensor" $CONFIG_PATH)
+export access_token=$(jq --raw-output ".access_token" $CONFIG_PATH)
 export auto_purge=$(jq --raw-output ".auto_purge" $CONFIG_PATH)
 export SSH_OPT="ssh -i ~/.ssh/id_ed25519 -o StrictHostKeyChecking=no"
 
@@ -13,6 +15,7 @@ bashio::log.info "A public/private key pair was generated for you."
 bashio::log.notice "Please use this public key on the backup server:"
 bashio::log.notice "${PUBLIC_KEY}"
 
+error=0
 rsyncurl="$user@$server:$directory"
 
 bashio::log.info "Uploading backup to to $rsyncurl ..."
@@ -22,8 +25,19 @@ rsync -av -e "$SSH_OPT" /backup/ $rsyncurl \
 
 if [ $auto_purge -ge 1 ]; then
 	bashio::log.info "Start auto purge, keep last $auto_purge backups"
-	for file in `ls -t /backup/*.tar | awk "NR>$auto_purge"`;do echo "Purging file $file ...";rm $file; done || bashio::exit.nok "Could not prune backups."
+	for file in `ls -t /backup/*.tar | awk "NR>$auto_purge"`;do echo "Purging file $file ...";rm $file; done || error=1
 fi
+
+if [ $error -eq 1 ]; then
+	if [ -z "$access_token" ] && [ -z "$binary_sensor" ]
+ 		curl -X POST -H "Authorization: Bearer $access_token" \
+   		-H "Content-Type: application/json" \
+		-d '{"state": "off", "attributes": {"friendly_name": "Backup Rsync"}}' \
+    		http://homeassistant:8123/api/states/binary_sensor.$binary_sensor
+	fi
+ 	bashio::exit.nok "Could not prune backups."
+ fi
+
 
 bashio::log.info 'Finished rsync-backups'
 
